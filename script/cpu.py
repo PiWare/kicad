85ca431c20435e87249a15fe2e808511123ac709
Thomas Bernard <thomas@famillebernargouriou.fr>
2015-05-04 23:02:22 +0200

    Add some comments and started adapting the symbol class for reuse outside of chip components.

--- script/cpu.py
+++ script/cpu.py
@@ -45,6 +45,7 @@ import itertools
 import config
 from symbol import *
 
+# Dictionary to convert the csv pin type encoding to the kicad encoding.
 csvPinTypeToPinType = {
         "I" : "I",
         "O" : "O",
@@ -55,17 +56,24 @@ csvPinTypeToPinType = {
         }
 
 class Square(object):
+    """Schematic square object representation."""
     def __init__(self,x,y):
         self.x = x
         self.y = y
 
     def getRep(self, pins, symbolNameWidth, grp, nameCentered):
+        """Build a graphical representation able to contain all the pin names provided in the pins list.
+        Returns a tuple containing the representation of the square symbol followed by the computed bounderies.
+        """
+
+        # Determine the square width
         maxPinNameWidth = 0
         if len(pins["L"])>0:
             maxPinNameWidth = max(maxPinNameWidth, max(x.length for x in pins["L"]))+cfg.SYMBOL_PIN_TEXT_OFFSET
         if len(pins["R"])>0:
             maxPinNameWidth = max(maxPinNameWidth, max(x.length for x in pins["R"]))+cfg.SYMBOL_PIN_TEXT_OFFSET
 
+        # determine the square height
         maxPinNameHeight = 0
         if len(pins["U"])>0:
             maxPinNameHeight = max(maxPinNameHeight, max(x.length for x in pins["U"]))+cfg.SYMBOL_PIN_TEXT_OFFSET
@@ -76,6 +84,8 @@ class Square(object):
 
         width = max([symbolNameWidth, maxPinNameWidth, maxPinsHoriz*cfg.SYMBOL_GRID*3])
         height = max(maxPinNameHeight, maxPinsVert*cfg.SYMBOL_GRID*3)+cfg.SYMBOL_TEXT_MARGIN
+
+        # Add a bit of space between the pins and the border of the outline
         if len(pins["U"]) > 0 and len(pins["D"])>0:
             height = height + cfg.SYMBOL_TEXT_MARGIN
 
@@ -92,6 +102,7 @@ class Square(object):
         return "S %i %i %i %i %i 1 %i N"%(x, y, x+width, y+height, grp, cfg.SYMBOL_LINE_WIDTH), [x,y,x+width,y+height]
 
 class Module(object):
+    """Represents a kicad schematic module instance."""
     PinOffset = {
             "U" : (0,-cfg.SYMBOL_PIN_LENGTH),
             "D" : (0,cfg.SYMBOL_PIN_LENGTH),
@@ -115,14 +126,21 @@ class Module(object):
 
 
     def __init__(self, representation, number):
+        """Create a new instance of a module.
+
+        representation -- object modeling the schematic representation of the module.
+        number -- module number
+        """
         self.representation = representation
         self.number = number
         self.pins = { "U" : [], "L" : [], "R" : [], "D" : [] }
 
     def addPin(self, pin, orientation):
+        """Add a pin to the module with the given orientation."""
         self.pins[orientation].append(pin)
 
     def getPinRepList(self, orientation, xStart, yStart):
+        """Build the pin representation for the module. Laying out the pins around the symbol representation."""
         if self.number != 0:
             convert = 1
         else:
@@ -134,6 +152,7 @@ class Module(object):
         return [self.pins[orientation][x].getRep(xStart+startOffset[0]+x*pinStep[0]+pinOffset[0], yStart+startOffset[1]+x*pinStep[1]+pinOffset[1], orientation, self.number, convert) for x in pinRange]
 
     def getRep(self, symbolName, symbolNameXPos, nameCentered):
+        """Build the module representation including all the pins the name and the symbol reference."""
         symbolRep, symbolOutline = self.representation.getRep(self.pins, symbolNameXPos+len(symbolName)*cfg.SYMBOL_NAME_SIZE/2, self.number, nameCentered)
         return ([symbolRep]
                 + self.getPinRepList("U",symbolOutline[0],symbolOutline[1])
@@ -142,9 +161,12 @@ class Module(object):
                 + self.getPinRepList("R",symbolOutline[0],symbolOutline[1]))
 
 def ReadHeader(reader):
+    """Read the header lines from the csv reader."""
+
     result = {}
     for row in reader:
         if len(row)>0:
+            # The end of the header is marked by "Number" in the first column.
             if row[0] != 'Number':
                 result[row[0]]=row[1]
             else:
@@ -162,9 +184,12 @@ def MakeMultiSymbol(inFile, outFile):
     vddGrp = []
     catchName = re.compile('\A[a-zA-Z]+')
     findGnd = re.compile('(GND)|(VSS)')
+    # Open the chip input file
     with open(inFile, 'rb') as csvfile:
         reader = csv.reader(csvfile)
+        # read the header of the symbol data
         header = ReadHeader(reader)
+        # read the pin names, types and descriptions.
         for row in reader:
             if len(row) > 0:
                 pinNum = int(row[0])
@@ -172,23 +197,29 @@ def MakeMultiSymbol(inFile, outFile):
                     pins[pinNum][1].append(row[1])
                 else:
                     pins[pinNum] = (csvPinTypeToPinType[row[2]],split(row[1],'/'))
+                    # for ground and power source pins, put them in a special list
                     if pins[pinNum][0] == 'W':
                         if findGnd.match(row[1]):
                             gndGrp.append(pinNum)
                         else:
                             vddGrp.append(pinNum)
                     else:
+                        # build the pin grouping dictionary
                         portGroupName = catchName.search(row[1]).group(0)
                         if portGroupName in portGroups:
                             portGroups[portGroupName].append(pinNum)
                         else:
                             portGroups[portGroupName] = [pinNum]
+    # Filter out the port groups containing at least 8 pins
     fullPortGrps = {}
     for grpName, grpPins in portGroups.iteritems():
         if len(grpPins) >= 8:
             fullPortGrps[grpName]=grpPins
+    # Remove all the port groups from the group dictionary
     for grpName, grpPins in fullPortGrps.iteritems():
         del portGroups[grpName]
+
+    # build port groups by concatenating incomplete groups together
     while len(portGroups) > 0:
         currentGrp = []
         for grpName, grpPins in portGroups.iteritems():
@@ -199,11 +230,13 @@ def MakeMultiSymbol(inFile, outFile):
             else:
                 currentGrp = currentGrp + grpPins
         del portGroups[grpName]
-    symbol = Symbol(header,"IC",False)
+    # Create the symbol for the chip
+    symbol = Symbol(header["Part"],"IC",False,header["Package"])
     # First take care of the power module
     powerModule = symbol.addModule(Module(Square(0,0),1))
     map(lambda pin : powerModule.addPin(Pin(string.join(pins[pin][1],'/'),pin,pins[pin][0]),"D"),vddGrp)
     map(lambda pin : powerModule.addPin(Pin(string.join(pins[pin][1],'/'),pin,pins[pin][0]),"U"),gndGrp)
+    # Add the port groups and modules
     grpIdx =2
     for grpName, grpPins in fullPortGrps.iteritems():
         newModule = symbol.addModule(Module(Square(0,0),grpIdx))
@@ -211,13 +244,14 @@ def MakeMultiSymbol(inFile, outFile):
         map(lambda pin : newModule.addPin(Pin(string.join(pins[pin][1],'/'),pin,pins[pin][0]),"R"),grpPins)
 
 
+    # write out the symbol to the output file. The list of strings needs to be assembled through a string join to add new line characters.
     outFile.write( string.join(symbol.getRep(),"\n" ) )
     outFile.write( "\n" )
 
 
 def MakeSingleSymbol(inFile, outFile):
     """ Output a new part in the outFile library.
-        The part will contain a single symbol.
+        The part will contain a single module.
     """
     startPins = False
     partName = ""
@@ -260,7 +294,7 @@ def MakeSingleSymbol(inFile, outFile):
     inGrp = pinGrps["I"] + pinGrps["B"]
     outGrp = pinGrps["O"]
 
-    symbol = Symbol(header,"IC",False)
+    symbol = Symbol(header["Part"],"IC",False,header["Package"])
     # First take care of the power module
     module = symbol.addModule(Module(Square(0,0),1))
     map(lambda pin : module.addPin(Pin(string.join(pins[pin][1],'/'),pin,pins[pin][0]),"D"),vddGrp)
@@ -297,7 +331,7 @@ def MakeRoundClockSymbol(inFile, outFile):
     grp2 = range(nbPins/4,nbPins/2)
     grp3 = range(nbPins/2,nbPins*3/4,1)
     grp4 = range(nbPins-1,nbPins*3/4-1,-1)
-    symbol = Symbol(header,"IC",True)
+    symbol = Symbol(header["Part"],"IC",True,header["Package"])
     # First take care of the power module
     module = symbol.addModule(Module(Square(0,0),1))
     map(lambda pin : module.addPin(Pin(pins[pin+1][1][0],pin,pins[pin+1][0]),"R"),grp1)
@@ -326,7 +360,7 @@ if __name__ == "__main__":
     output = open(args.output, "w")
     output.write("EESchema-LIBRARY Version 2.3\n")
 
-
+    # Process the data files according to the method selected by the command line arguments.
     if args.grouped != None:
         for src in args.grouped:
             MakeMultiSymbol(src, output)
--- script/symbol.py
+++ script/symbol.py
@@ -22,19 +22,37 @@ import config
 cfg = config.Config("config")
 
 class Pin(object):
+    """Represents a pin assigned to a schematic symbol."""
+
     FormatString = "X %s %i %i %i " + str(cfg.SYMBOL_PIN_LENGTH) + " %s " + str(cfg.SYMBOL_PIN_NUMBER_SIZE) + " " + str(cfg.SYMBOL_PIN_NAME_SIZE) + " %i %i %s"
 
     def __init__(self, name, number, type):
+        """Create a new pin object representing a symbol pin.
+
+        name -- name of the pin
+        number -- pin number
+        type -- type of the pin (specified as in the kicad schematic pin type)
+        """
         self.name = name
         self.length = len(name)*cfg.SYMBOL_PIN_NAME_SIZE
         self.type = type
         self.number = number
 
     def getRep(self,x,y,orientation,group,convert):
+        """Make a representation for the pin using the pin properties and the positioning information.
+
+        x -- x coordinate of the pin in the symbol representation
+        y -- y coordinate of the pin in the symbol representation
+        orientation -- one of 'U','D','L','R'
+        group -- group number which the pin is assigned to.
+        convert -- kicad convert (usually 2, or 0 for pins common to all modules)
+        """
         return Pin.FormatString %(self.name, self.number, x, y, orientation, group, convert, self.type)
 
 
 class Symbol(object):
+    """Represents a kicad schematic library symbol."""
+
     DefFormat="DEF %s %s 0 "+str(cfg.SYMBOL_PIN_TEXT_OFFSET)+" Y Y %i L N"
     RefFieldFormat = ( "F%i"%(cfg.REFERENCE_FIELD)
             + ' "%s" %i %i'
@@ -48,18 +66,29 @@ class Symbol(object):
 
     FootprintFieldFormat = "F%i"%(cfg.FOOTPRINT_FIELD) +' "%s" 0 0 30 H I C CCN'
 
-    def __init__(self, header, ref, nameCentered):
-        self.name = header["Part"]
-        self.footprint = header["Package"]
+    def __init__(self, name, ref, nameCentered, package = ""):
+        """Creates a new symbol instance.
+
+        name -- symbol name.
+        ref -- kicad symbol reference string.
+        nameCentered -- a boolean indicating if the name should be centered according to the symbol boundaries.
+        """
+        self.name = name
         self.ref = ref
         self.modules =[]
         self.nameCentered = nameCentered
+        self.footprint = package
 
     def addModule(self, module):
+        """Inserts a new module to the symbol. Returns the newly added module instance."""
         self.modules.append(module)
         return module
 
     def getRep(self):
+        """Build the symbol representation.
+
+        The result is a list of strings.
+        """
         if self.nameCentered == True:
             valueFieldXPos = -len(self.name)/4*cfg.SYMBOL_NAME_SIZE
             valueFieldYPos = cfg.SYMBOL_TEXT_MARGIN
